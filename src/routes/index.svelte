1647a94a3f2cdd1f09178ce0c2636773b50530e3
Ahmed DEMIAI <demiaiahmed@gmail.com>
2022-06-28 17:20:39 +0100

    Landing

---
+++ src/routes/index.svelte
@@ -0,0 +1,40 @@
+<div class=" mx-auto w-5/6 ">
+  <div class="w-3/4 mt-10 ml-3 lg:w-5/6">
+    <img src="recom-Logo-1.png" class="h-14" alt="" />
+  </div>
+  <div class="flex lg:flex-row mt-20 flex-col">
+    <div class="flex-auto lg:w-2/6 font-montserrat mt-20 mb-10">
+      <h1 class="sm:text-3xl text-2xl font-bold  text-left">
+        The new Shopify for Cash On Delivery
+      </h1>
+      <p class="mt-10 text-zinc-400 sm:text-lg text-sm ">
+        Recom’s store builder, orders manager, confirmation center, package
+        tracking and stock manager helps you build, manage and scale your
+        business from one place with no effort.
+      </p>
+      <div class="flex sm:flex-row mt-10 flex-col">
+        <div class="flex-inital">
+          <input
+            type="email"
+            name="Email"
+            placeholder="Enter your email here"
+            class="px-2 py-2 border-2 border-violet-600 focus:border-green-600 focus:outline-none rounded w-64"
+          />
+        </div>
+        <div class="flex-initial sm:mt-0 mt-5 ml-20 sm:ml-0">
+          <button
+            type="button"
+            class="w-32 bg-violet-500 py-2.5 ml-2 text-white px-6 rouned-md shadow-md hover:bg-violet-400"
+            >Email Me
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="flex-auto w-full">
+      <img src="demo.png" alt="" class=" mx-auto" />
+    </div>
+  </div>
+  <div class="w-full items-center mt-20">
+    <p class="text-center">Recom - Ecommerce Solution 2022</p>
+  </div>
+</div>
